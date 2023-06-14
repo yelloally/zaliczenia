6d433e47aea9b007f67afbad0163db1bfeb1d751
Alona <122108963+yelloally@users.noreply.github.com>
2023-06-14 11:07:37 +0200

    Update project.py

--- project.py
+++ project.py
@@ -2,6 +2,8 @@ import argparse
 import json
 import yaml
 import xml.etree.ElementTree as ET
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('input_file', help='Ścieżka do pliku wejściowego')
@@ -54,3 +56,43 @@ def write_xml_file(data, file_path):
     tree = ET.ElementTree(root)
     tree.write(file_path, encoding='utf-8', xml_declaration=True)
     print("Dane zapisane do pliku XML.")
+
+
+class ConverterApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Konwerter danych")
+        self.setGeometry(100, 100, 300, 150)
+
+        self.input_label = QLabel("Plik wejściowy:", self)
+        self.input_field = QLineEdit(self)
+
+        self.output_label = QLabel("Plik wyjściowy:", self)
+        self.output_field = QLineEdit(self)
+
+        self.convert_button = QPushButton("Konwertuj", self)
+        self.convert_button.clicked.connect(self.convert_data)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.input_label)
+        layout.addWidget(self.input_field)
+        layout.addWidget(self.output_label)
+        layout.addWidget(self.output_field)
+        layout.addWidget(self.convert_button)
+
+        widget = QWidget(self)
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+    def convert_data(self):
+        input_file = self.input_field.text()
+        output_file = self.output_field.text()
+
+        print("Konwersja zakończona.")
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = ConverterApp()
+    window.show()
+    sys.exit(app.exec())
